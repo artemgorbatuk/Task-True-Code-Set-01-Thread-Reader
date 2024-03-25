15336f31f3a74834204cfaa140912a7fed1b98ba
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-03-26 01:51:43 +0300

    server console : логики создана

--- Server.Console/Program.cs
+++ Server.Console/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using Api;
+
+var server = new ServerService();
+
+try
+{
+    server.StartServer(() =>
+    {
+        Console.WriteLine("The server is running.");
+        Console.WriteLine();
+    });
+
+    await foreach (var message in server.ReadSreamAsync((byte)'\n'))
+    {
+        Console.WriteLine($"Message has been gotten: {message}");
+    }
+}
+catch (Exception exception)
+{
+    Console.WriteLine(exception.Message);
+}
+finally
+{
+    server.StopServer(() =>
+    {
+        Console.WriteLine();
+        Console.WriteLine("The server was stopped");
+    });
+}
+
+Console.ReadKey();
